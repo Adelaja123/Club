c36f5bcef6b73d72fef90355231b7d0909572c86
Oluwagbotemi Adelaja <adelaja123@example.com>
2026-04-17 03:30:55 -1100

    Update redirect configuration formatting

--- next.config.mjs
+++ next.config.mjs
@@ -9,9 +9,9 @@ const nextConfig = {
   redirects: async () => [
     {
       source: "/:path*",
+      has: [{ type: "host", value: "www.oluwagbotemi.space" }],
       destination: "https://oluwagbotemi.space/:path*",
       permanent: true,
-      has: [{ type: "host", value: "www.oluwagbotemi.space" }],
     },
   ],
 };
